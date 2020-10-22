ec4a3ef9b70897cc6912a1f05ab168aaa942d594
mnroesch42 <mnroesch42@students.tntech.edu>
2020-10-22 14:24:25 -0500

    Added background image

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
+    path("aboutme", views.aboutme, name="aboutme"),
 ]
--- website/views.py
+++ website/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
-    return render(request, "website/layout.html", {
+    return render(request, "website/homepage.html", {
        
+    })
+
+def aboutme(request):
+    return render(request, "website/aboutme.html", {
+
     })
