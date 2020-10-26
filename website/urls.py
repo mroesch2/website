4b1b3d8c40d477b19dbaf6fad23133bc207d42fd
mnroesch42 <mnroesch42@students.tntech.edu>
2020-10-26 14:57:26 -0500

    added url path to about me

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.homepage, name="homepage"),
+    path("homepage", views.homepage, name="homepage"),
     path("aboutme", views.aboutme, name="aboutme"),
 ]
